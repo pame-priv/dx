ca9c412e5e4c88b3805f36566d33e60de6b0045c
pame-priv <c.htkc.2210@gmail.com>
2026-01-28 05:15:24 +0900

    Add files via upload

---
+++ Chrome拡張機能/統合業務ツール/business_config.js
@@ -0,0 +1,18 @@
+export const businessConfig = [
+    {
+      id: "gyomuA",
+      name: "業務A",
+      configPath: "業務A/menu_config.js"
+    },
+    {
+      id: "gyomuB",
+      name: "業務B",
+      configPath: "業務B/menu_config.js"
+    },
+    {
+      id: "gyomuC",
+      name: "業務C",
+      configPath: "業務C/menu_config.js"
+    }
+  ];
+  
